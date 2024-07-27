c0890f990f4172c0e026759539732b1d2caedb63
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-07-27 10:11:06 +0200

    Ajout d'un filtre sur le end point worth

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,22 @@ class ItemsController < ApplicationController
     @items = Item.all
     render json: @items
   end
+
+  def worth
+    # récupérer les items dont le type de ressource appartient à un certain type
+    @items = Item.where(ressource_type: RessourceTypes.types)
+
+    #verifier si la mediane est supérieur au cout du dernière item
+    newItem = @items.select do |item|
+        item.isWorth
+    end
+
+    render json: newItem
+  end
 end
+
+
+private
+
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  include ArrayUtils
+
+  def isWorth
+    condition1 = calculate_median(unit_price) > (unit_price.last + calculate_median(unit_price) * 0.02)
+    condition2 = calculate_median(tenth_price) > (tenth_price.last + calculate_median(tenth_price) * 0.02)
+    condition3 = calculate_median(hundred_price) > (hundred_price.last + calculate_median(hundred_price) * 0.02)
+    return condition1 || condition2 || condition3
+  end
 end
+  
